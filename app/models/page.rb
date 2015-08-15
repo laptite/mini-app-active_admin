4542756b293cc0eb4aefc211c62e2d2cf23a7a72
laptite <laptite@gmail.com>
2015-08-14 20:25:19 -0400

    toggles published to enforce single selection

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,19 @@
 class Page < ActiveRecord::Base
-  has_many :sections, -> { order(position: :asc) }
+  has_many :sections
   
   accepts_nested_attributes_for :sections
 
   scope :published, -> { where(published: true) }
+
+  before_save :toggle_published
+
+  def toggle_published
+    if published_changed? && other_pages_are_published?
+      Page.published.first.update_column(:published, !published)
+    end
+  end
+
+  def other_pages_are_published?
+    Page.all.reject{ |p| p == self }.any?{ |c| c.published? }
+  end
 end
